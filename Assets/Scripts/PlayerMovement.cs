a08ee5ce26fbb065a18e411654e1612a3aa4c603
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-08-20 00:36:53 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Speed = 10f;
+    [Range(0, 1)]
+    public float AirSpeedMultiplier = 0.3f;
+
+    public float MaxJumpCharge = 10f;
+    public float JumpChargePerSecond = 5f;
+    public float JumpTapCutoff = 4f;
+    public float JumpTapForce = 7f;
+    private float currentJumpCharge = 0f;
+
+    Rigidbody rb;
+
+    public LayerMask Ground;
+    public Camera cam;
+
+    bool grounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (cam == null) cam = Camera.main;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            currentJumpCharge += currentJumpCharge <= MaxJumpCharge ? JumpChargePerSecond * Time.deltaTime : 0;
+            Debug.LogFormat("Charging {0}", currentJumpCharge);
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            if (grounded)
+            {
+                if (currentJumpCharge > JumpTapCutoff)
+                {
+                    rb.AddForce(Vector3.up * currentJumpCharge, ForceMode.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(Vector3.up * JumpTapForce, ForceMode.Impulse);
+                }
+            }
+            currentJumpCharge = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, 1, Ground);
+
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement;//= new Vector3(HorizontalInput, 0, VerticalInput);
+        movement = cam.transform.forward * VerticalInput;
+        movement += cam.transform.right * HorizontalInput;
+
+        movement *= Speed;
+
+        if (!grounded)
+        {
+            movement *= AirSpeedMultiplier;
+        }
+
+        rb.AddForce(movement);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    public float height = 1f;
+    public float distance = 2f;
+
+    public bool hideBlockingObjects = true;
+
+    private Vector3 offsetX;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        offsetX = new Vector3(0, height, distance);
+    }
+
+    Dictionary<Renderer, float> renders = new Dictionary<Renderer, float>();
+
+    void LateUpdate()
+    {
+        float RotateAxis = Input.GetAxis("RotateCamera") * 10 * Time.deltaTime;
+
+        offsetX = Quaternion.AngleAxis(RotateAxis * turnSpeed, Vector3.up) * offsetX;
+        transform.position = player.position + offsetX;
+        transform.LookAt(player.position);
+
+        if (hideBlockingObjects)
+        {
+            Vector3 direction = transform.position - player.position;
+            //direction.Normalize();
+            
+            RaycastHit hit;
+
+            Debug.DrawRay(transform.position, -direction, Color.red);
+
+            if (Physics.Raycast(transform.position, -direction, out hit))
+            {
+                if (hit.collider.tag != "Player")
+                {
+                    Renderer r = hit.collider.gameObject.GetComponent<Renderer>();
+
+                    if (r)
+                    {
+                        foreach (Material m in r.materials)
+                        {
+                            if (!renders.ContainsKey(r))
+                            {
+                                renders.Add(r, m.color.a);
+                                Debug.Log(m.GetFloat("_Mode"));
+                                m.SetFloat("_Mode", 2);
+                                Debug.LogFormat("_SrcBlend: {0}",m.GetInt("_SrcBlend"));
+                                m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                                Debug.LogFormat("_DstBlend: {0}", m.GetInt("_DstBlend"));
+                                m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                                Debug.LogFormat("_ZWrite: {0}", m.GetInt("_ZWrite"));
+                                m.SetInt("_ZWrite", 0);
+                                m.DisableKeyword("_ALPHATEST_ON");
+                                m.EnableKeyword("_ALPHABLEND_ON");
+                                m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                                Debug.Log(m.renderQueue);
+                                m.renderQueue = 3000;
+                                m.color = new Color(m.color.r, m.color.g, m.color.b, m.color.a * 0.5f);
+                                Debug.LogFormat("Alpha: {0}", m.color.a);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    List<Renderer> toRemove = new List<Renderer>();
+                    foreach (Renderer r in renders.Keys)
+                    {
+                        try
+                        {
+                            Debug.Log("Removing: " + r.ToString());
+                            foreach (Material m in r.materials)
+                            {
+                                m.SetFloat("_Mode", 0);
+                                m.SetInt("_SrcBlend", 1);
+                                m.SetInt("_DstBlend", 0);
+                                m.SetInt("_ZWrite", 1);
+                                m.EnableKeyword("_ALPHATEST_ON");
+                                m.DisableKeyword("_ALPHABLEND_ON");
+                                m.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                                m.renderQueue = 2000;
+                                m.color = new Color(m.color.r, m.color.g, m.color.b, renders[r]);
+                            }
+                            toRemove.Add(r);
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            //Do nothing
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogErrorFormat("Un-expectected exception: {0}", e.ToString());
+                        }
+                        
+                    }
+
+                    foreach(Renderer r in toRemove)
+                    {
+                        renders.Remove(r);
+                    }
+                    toRemove.Clear();
+                }
+            }
+        }
+    }
+}
