ddc04b88008151fdd7c5de2af52cd463318a2413
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-08-20 17:35:16 +0100

    Added Utils.cs

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Utils
+{
+    public static bool RoughlyEqual(float a, float b)
+    {
+        return RoughlyEqual(a, b, 1);
+    }
+
+    public static bool RoughlyEqual (float a, float b, float threshold)
+    {
+        return (Math.Abs(a - b) < threshold);
+    }
+
+    public static bool VectorRoughlyEqual(Vector3 a, Vector3 b)
+    {
+        return VectorRoughlyEqual(a, b, 1);
+    }
+
+    public static bool VectorRoughlyEqual (Vector3 a, Vector3 b, float threashold)
+    {
+        return (RoughlyEqual(a.x, b.x, threashold) && 
+            RoughlyEqual(a.y, b.y, threashold) && 
+            RoughlyEqual(a.z, b.z, threashold));
+    }
+}
